be677dbc4a5280a2958a79fcce7f77255afb1e3c
Jay Raj <iamgjraj@gmail.com>
2025-04-09 18:22:06 -0500

    added profile

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -16,3 +16,6 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const profileRoutes = require('./routes/profileRoutes');
+app.use('/api/profiles', profileRoutes);
